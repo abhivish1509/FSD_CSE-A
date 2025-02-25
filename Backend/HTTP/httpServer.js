19ccb1d97d95d855e9f28ef4f1609561c4124147
abhivish1509 <abhishekvishwakarma1509@gmail.com>
2025-02-25 14:31:02 +0530

    comitted

--- Backend/HTTP/httpServer.js
+++ Backend/HTTP/httpServer.js
@@ -1,11 +1,20 @@
 const http = require('http');
 const port = 3000;
 
-const server = http.createServer((req,res)=>{
-    res.setHeader('content-type',"application/json");
-    res.end("<h1>welcome to the server</h1>");
-})
+const server = http.createServer(async (req, res) => {
+    if (req.url === "/msg" && req.method === "GET") {
+        res.setHeader('Content-Type', 'text/html');
+        res.end("<h2>Hello there!!</h2>");
+        return;
+    }
+    
+    else if (req.url === "/" && req.method === "GET") {
+        res.setHeader('Content-Type', 'text/html');
+        res.end("<h2>Welcome to IIT Lalkaun</h2>");
+        return;
+    }
+});
 
-server.listen(port,()=>{
-    console.log("Server is running on port" + port);
-})
+server.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
